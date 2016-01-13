f3f877cc32e2dbfe23a928492b39824e9a10fe08
Ernesto Soliani <ernestosoliani@gmail.com>
2016-01-13 18:21:54 -0300

    add pie chart

---
+++ app/scripts/components/charts/health-pie-chart.jsx
@@ -0,0 +1,76 @@
+import React, { PropTypes } from 'react';
+import T from '../misc/t';
+import ShouldRenderMixin from '../../utils/should-render-mixin';
+import HighCharts from 'highcharts';
+
+require('highcharts/modules/exporting')(HighCharts);
+require('stylesheets/charts/health-facilities-barchar');
+require('highcharts/highcharts-3d')(HighCharts);
+
+const HealthPieChart = React.createClass({
+  propTypes: {
+    data: PropTypes.object.isRequired,
+    divId: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  },
+
+  mixins: [ShouldRenderMixin],
+
+  componentDidMount() {
+    this.getChart();
+  },
+
+  getChart() {
+    const keys = Object.keys(this.props.data);
+    if (keys.length === 0) {
+      return false;
+    }
+    const data = keys
+      .filter(key => key !== 'total')
+      .map(key => [key, this.props.data[key]])
+      .sort((a, b) => b[1] - a[1]);
+    const stats = [{
+      name: 'Health Facilities',
+      data: data,
+    }];
+   // needs translations
+    return new HighCharts.Chart({
+      chart: {
+        type: 'pie',
+        options3d: {
+          enabled: true,
+          alpha: 45,
+        },
+        renderTo: this.props.divId,
+      },
+
+      title: {
+        text: '',
+      },
+
+      plotOptions: {
+        pie: {
+          innerSize: 100,
+          depth: 45,
+        },
+      },
+
+      series: stats,
+    });
+  },
+
+
+  render() {
+    if (Object.keys(this.props.data) === 0) {
+      return (<div>empty</div>);
+    }
+    return (
+      <div className="health-facilities-piechart">
+        <h3 className="chart-title"><T k={this.props.title} /></h3>
+        <div className="chart-container" id={this.props.divId}></div>
+      </div>
+    );
+  },
+});
+
+export default HealthPieChart;
