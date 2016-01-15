dda42e6555962daae0e66351b5a9fe72e11a05d2
Ernesto Soliani <ernestosoliani@gmail.com>
2016-01-15 20:44:42 -0300

    add more metrics on left panel

--- app/scripts/components/right-panel/deliveries-right-panel.jsx
+++ app/scripts/components/right-panel/deliveries-right-panel.jsx
@@ -4,6 +4,8 @@ import ViewModes from '../../constants/view-modes';
 import MetricSummary from '../charts/metric-summary-chart';
 import { Result } from '../../utils/functional';
 
+const metrics = ['TOTAL', 'HEALTH FACILITY DELIVERIES', 'TRADITIONAL BIRTH ATTENDANTS (TBA)', 'ANTENATAL CARE PROJECTION', 'BORN BEFORE ARRIVAL (BBA)', 'HOME DELIVERY'];
+
 const DeliveriesRightPanel = React.createClass({
   propTypes: {
     children: PropTypes.node, // injected
@@ -12,16 +14,22 @@ const DeliveriesRightPanel = React.createClass({
     setSelected: PropTypes.func,
     viewMode: PropTypes.instanceOf(ViewModes.OptionClass),  // injected
   },
-  getDeliveries() {
-    return Result.sumBy(this.props.data, 'TOTAL').TOTAL;
+  getDeliveries(stats, m) {
+    return stats[m].value || 0;
   },
 
   render() {
+    const stats = Result.sumByAll(this.props.data, metrics);
     return (
       <div className="container other-selections">
         <div className="row">
-          <MetricSummary icon="deliveries.png" metric={this.getDeliveries()} title="chart.deliveries.title"/>
+          <MetricSummary icon="deliveries.png" metric={Math.round(stats.TOTAL.value / stats.TOTAL.total)} title={`chart.deliveries-average.title`}/>
         </div>
+        {metrics.map(m => (
+          <div className="row">
+            <MetricSummary icon="deliveries.png" metric={this.getDeliveries(stats, m)} title={`chart.deliveries-${m}.title`}/>
+          </div>
+        ))}
       </div>);
   },
 });
