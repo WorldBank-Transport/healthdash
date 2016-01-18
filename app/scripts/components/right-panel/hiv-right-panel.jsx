f76fa94f89bf228e92c6cf0c715aa71be9c0e115
Ernesto Soliani <ernestosoliani@gmail.com>
2016-01-18 12:21:24 -0300

    add more stats on right panel

--- app/scripts/components/right-panel/hiv-right-panel.jsx
+++ app/scripts/components/right-panel/hiv-right-panel.jsx
@@ -1,7 +1,10 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'reflux';
 import DataTypes from '../../constants/data-types';
 import ViewModes from '../../constants/view-modes';
 import MetricSummary from '../charts/metric-summary-chart';
+import PopulationStore from '../../stores/population';
+import { Result } from '../../utils/functional';
 
 
 const HivRightPanel = React.createClass({
@@ -13,11 +16,28 @@ const HivRightPanel = React.createClass({
     viewMode: PropTypes.instanceOf(ViewModes.OptionClass),  // injected
   },
 
+  mixins: [
+    connect(PopulationStore, 'population'),
+  ],
+
+  getPopulation() {
+    return Result.sumBy(this.state.population, 'TOTAL').TOTAL;
+  },
+
   render() {
+    if (this.props.data.length === 0) {
+      return false;
+    }
     return (
       <div className="container other-selections">
         <div className="row">
           <MetricSummary icon="hiv.png" metric={this.props.data.length} title="chart.hiv.title"/>
+        </div>
+        <div className="row">
+          <MetricSummary icon="hiv.png" metric={this.props.data.length / 25} title="chart.hiv-region.title"/>
+        </div>
+        <div className="row">
+          <MetricSummary icon="workers.png" metric={Math.round(this.getPopulation() / this.props.data.length)} title="chart.hiv-people.title"/>
         </div>
       </div>);
   },
