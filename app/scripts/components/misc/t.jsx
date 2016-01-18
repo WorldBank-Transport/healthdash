3e15a41a40401a05a6fc449df38bb72e4731539e
galina kalvatcheva <galinagk@gmail.com>
2016-01-18 14:01:31 -0500

    TSD 302 text change for print

--- app/scripts/components/misc/t.jsx
+++ app/scripts/components/misc/t.jsx
@@ -234,7 +234,7 @@ const allTranslations = {
 
     'share.share': 'Share',
     'share.feedback': 'Feedback',
-    'share.print': 'Print',
+    'share.print': 'PDF',
 
     'error': 'We\'re sorry',
     'error.retry': 'Retry',
