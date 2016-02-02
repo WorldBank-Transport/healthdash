234cc9f5a59e7c00a13d6d5ac7777c8815c726d8
Diego <ddimunzio@developementgateway.org>
2016-02-02 09:38:45 -0300

    Remove underline

--- app/scripts/components/charts/health-facilities-barchar.jsx
+++ app/scripts/components/charts/health-facilities-barchar.jsx
@@ -67,6 +67,7 @@ const HealthFacilitiesChart = React.createClass({
     const categories = Object.keys(regions).filter(key => key !== 'total').sort((a, b) => regions[b] - regions[a]);
     const drillDown = this.calculateDrillDown(categories, this.props.data);
     const stats = this.parseData(facilitiesStats, categories);
+
    // needs translations
     this.chart = new HighCharts.Chart({
       chart: {
@@ -92,13 +93,14 @@ const HealthFacilitiesChart = React.createClass({
         useHTML: true,
       },
 
-      plotOptions: {
+        plotOptions: {
         column: {
           stacking: 'normal',
           dataLabels: {
             color: (HighCharts.theme && HighCharts.theme.dataLabelsColor) || 'white',
             style: {
               textShadow: '0 0 3px black',
+              textDecoration  : 'none',
             },
           },
         },
@@ -107,6 +109,10 @@ const HealthFacilitiesChart = React.createClass({
       series: stats,
       drilldown: {
         series: drillDown,
+        activeAxisLabelStyle: {
+          cursor: 'pointer',
+          textDecoration: 'none'     
+        }
       },
     });
     return this.chart;
