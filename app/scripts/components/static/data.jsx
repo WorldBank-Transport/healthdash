6ff4d835d301b55ba4fc158701dbd39d960c33ce
Ernesto Soliani <ernestosoliani@gmail.com>
2016-03-22 23:16:20 -0300

    TSD-447 change data page

--- app/scripts/components/static/data.jsx
+++ app/scripts/components/static/data.jsx
@@ -97,7 +97,15 @@ const Data = React.createClass({
             <div className="content-section" id="DataSources">
               <h3><T k="static.data-souces.title" /></h3>
               <ul className="dataset-links">
-                <li><Icon type="link"/><T k="static.dataset.links" /><a href="http://www.opendata.go.tz/organization/ministry-of-health-and-social" target="_blank">http://www.opendata.go.tz/organization/ministry-of-health-and-social</a></li>
+                <li id="health"><Icon type="link"/><a href="http://www.opendata.go.tz/dataset/list-of-health-facilities-with-geographical-location" target="_blank"><T k="static.link.facilities" /></a></li>
+                <li id="death"><Icon type="link"/><a href="http://www.opendata.go.tz/dataset/number-and-causes-of-death-occured-by-region" target="_blank"><T k="static.link.deaths" /></a></li>
+                <li id="family-planing"><Icon type="link"/><a href="http://www.opendata.go.tz/dataset/idadi-na-asilimia-ya-wateja-wa-huduma-ya-uzazi-wa-mpango-kwa-mikoa" target="_blank"><T k="static.link.family-planning" /></a></li>
+                <li id="deliveries"><Icon type="link"/><a href="http://www.opendata.go.tz/dataset/idadi-ya-wanaojifungulia-kwenye-vituo-vya-kutolea-huduma-za-afya-na-sehemu-zingine" target="_blank"><T k="static.link.deliveries" /></a></li>
+                <li id="workers"><Icon type="link"/><a href="http://www.opendata.go.tz/dataset/idadi-ya-wafanyakazi-wa-afya-kwa-mikoa" target="_blank"><T k="static.link.workers" /></a></li>
+                <li id="ipd"><Icon type="link"/><a href="http://www.opendata.go.tz/dataset/idadi-ya-magonjwa-kutoka-idara-ya-wagonjwa-waliolazwa-kwa-mikoa" target="_blank"><T k="static.link.ipd" /></a></li>
+                <li id="opd"><Icon type="link"/><a href="http://www.opendata.go.tz/dataset/idadi-ya-magonjwa-kutoka-idara-ya-wagonjwa-wa-nje-kwa-mikoa" target="_blank"><T k="static.link.opd" /></a></li>
+                <li id="tetanus"><Icon type="link"/><a href="http://www.opendata.go.tz/dataset/number-and-percentage-of-pregnant-women-received-two-or-more-tetanus-toxoid-vaccine-tt2-by-region" target="_blank"><T k="static.link.tetanous" /></a></li>
+                <li id="hiv"><Icon type="link"/><a href="http://www.opendata.go.tz/dataset/list-of-hiv-care-and-treatment-centre-ctc-by-district" target="_blank"><T k="static.link.hiv" /></a></li>
               </ul>
             </div>
           </div>
